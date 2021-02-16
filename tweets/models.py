57c220c6afbb7f4924e214913574e344e5406748
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-02-16 20:28:43 +0500

    Create Comment model

--- tweets/models.py
+++ tweets/models.py
@@ -9,3 +9,16 @@ class TweetModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True)
     username = models.CharField(max_length=100, blank=True)
+
+
+class Comment(models.Model):
+    tweet = models.ForeignKey(TweetModel, on_delete=models.CASCADE, related_name='comment')
+    username = models.CharField(max_length=255)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('created', 'username')
+
+    def __str__(self):
+        return f'Comment by {self.username} to {self.tweet.owner}'

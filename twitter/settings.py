0b5c4ba9d59ec278cde4b15369a07678565b21b2
KhusniddinovUz <khusniddinovuz@gmail.com>
2022-06-26 21:22:25 +0500

    change email host password

--- twitter/settings.py
+++ twitter/settings.py
@@ -149,6 +149,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'twittercloneuz@gmail.com'
-EMAIL_HOST_PASSWORD = 'tveqlbrxvpsfzdcn'
+EMAIL_HOST_PASSWORD = 'bskilexkpkjyeefs'
 
 django_heroku.settings(locals())

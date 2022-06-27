4787594a4c19df2da546fbc8944200818dc6a286
KhusniddinovUz <khusniddinovuz@gmail.com>
2022-06-27 19:45:04 +0500

    update app password

--- twitter/settings.py
+++ twitter/settings.py
@@ -145,10 +145,11 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'twittercloneuz@gmail.com'
-EMAIL_HOST_PASSWORD = 'bskilexkpkjyeefs'
+EMAIL_HOST_PASSWORD = 'jpcowueqpztbzeva'
 
 django_heroku.settings(locals())
